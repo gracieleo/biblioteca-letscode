9ebe54275012d1ee5a7a365bbe4b13247da3abd4
Graciele de Oliveira <oliveiragracie@gmail.com>
2021-10-03 16:06:23 -0300

    add lombok, isbn e editora

---
+++ biblioteca-letscode/src/main/java/br/com/letscode/biblioteca/Feriados.java
@@ -0,0 +1,13 @@
+package br.com.letscode.biblioteca;
+
+import lombok.Value;
+
+import java.time.LocalDate;
+
+@Value
+
+    public class Feriados {
+        LocalDate data;
+        String descricao;
+    }
+
